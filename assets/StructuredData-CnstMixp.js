83441365398a5bc51b8c8b9e72590924b78c63a4
Mehdi Nezhadrasouli <mehdi@Mehdis-MacBook-Air.local>
2026-08-22 23:41:39 +0330

    Publish Mehdi Human Stack

---
+++ assets/StructuredData-CnstMixp.js
@@ -0,0 +1,1 @@
+import{v as e}from"./index-1nn4eZhR.js";var t=e();function n({data:e}){let n=JSON.stringify(e).replace(/</g,`\\u003c`);return(0,t.jsx)(`script`,{type:`application/ld+json`,dangerouslySetInnerHTML:{__html:n}})}export{n as t};
