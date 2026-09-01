cb875ced6356fb3562733bdb09bddbd8ebef0fad
Mehdi Nezhadrasouli <mehdi@Mehdis-MacBook-Air.local>
2026-09-01 15:04:38 +0330

    Connect discovery conversations to Calendly

---
+++ assets/StructuredData-CCFabIdq.js
@@ -0,0 +1,1 @@
+import{x as e}from"./site-B9G3thRN.js";var t=e();function n({data:e}){let n=JSON.stringify(e).replace(/</g,`\\u003c`);return(0,t.jsx)(`script`,{type:`application/ld+json`,dangerouslySetInnerHTML:{__html:n}})}export{n as t};
